9b610ed053abf5010fa48ae9f673adb6393b8986
Max Base <MaxBaseCode@Gmail.Com>
2025-05-14 08:29:09 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify, render_template_string
 from datetime import datetime
 import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+PORT = int(os.getenv("SERVER_PORT", 5000))
 
 logging.basicConfig(filename='camera_events.log',
                     level=logging.INFO,
@@ -47,4 +53,4 @@ def status():
     return render_template_string(html)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=PORT)
